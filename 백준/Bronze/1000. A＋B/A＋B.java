faadbf52f6185f7f6ac342b822202ed34d41e0f8
sohee <144431880+MisaSohee@users.noreply.github.com>
2023-12-06 00:22:18 +0900

    [Bronze V] Title: A+B, Time: 204 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 문제의 요구사항에 맞게 코드를 작성합니다.
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+
+        System.out.println(A + B);
+    }
+}
