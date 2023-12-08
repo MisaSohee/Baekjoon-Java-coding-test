1e77c6b516539f630f44269ad592a69c5203e761
sohee <144431880+MisaSohee@users.noreply.github.com>
2023-12-09 01:36:06 +0900

    [Bronze V] Title: 고양이, Time: 120 ms, Memory: 13896 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+import static java.lang.Integer.*;
+import java.util.InputMismatchException;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
