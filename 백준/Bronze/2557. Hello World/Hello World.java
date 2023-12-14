8fa04369dad7dc5b81fa96088d3cb2350423c381
sohee <144431880+MisaSohee@users.noreply.github.com>
2023-12-14 23:56:48 +0900

    [Bronze V] Title: Hello World, Time: 116 ms, Memory: 13940 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static java.lang.Integer.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
